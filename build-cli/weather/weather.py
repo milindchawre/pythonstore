2a0ea35483297cfaf9fbd64160da80ca7c850627
Milind Chawre <milindchawre@gmail.com>
2020-10-30 19:28:39 +0530

    Removing api key

--- build-cli/weather/weather.py
+++ build-cli/weather/weather.py
@@ -11,7 +11,6 @@ parser.add_argument('--country', default='us', help='country zip/postal belongs 
 
 args = parser.parse_args()
 
-#OWM_API_KEY=24b5862a3f90e13e413196165f37b02e
 api_key = os.getenv('OWM_API_KEY')
 
 if not api_key:
